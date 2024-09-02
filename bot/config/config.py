1dda1f51793549124f56595dfb98e00a963c96ee
J8binance <69707734+J8binance@users.noreply.github.com>
2024-09-02 15:33:19 +0800

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
 
     API_ID: int
     API_HASH: str
-
+    max_session_connect_delay: int = 60  # Default value of 60 seconds
     SLEEP_BETWEEN_CLAIM: list[int] = [10, 20]
     SLEEP_BETWEEN_TASK_CLAIM: list[int] = [5, 10]
     SLEEP_BETWEEN_FARMING: list[int] = [10, 20]
